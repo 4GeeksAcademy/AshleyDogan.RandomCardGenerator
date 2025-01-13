17b924efaa5277ad94b3def4ea56848e3434bfa3
Ashley Dogan <a.a.rappette@gmail.com>
2025-01-13 20:45:12 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,50 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const suits = ["♦", "♥", "♠", "♣"];
+const ranks = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+function getRandomSuit() {
+  const randomSuit = Math.floor(Math.random() * suits.length);
+  return suits[randomSuit];
+}
+
+function getRandomRank() {
+  const randomRank = Math.floor(Math.random() * ranks.length);
+  return ranks[randomRank];
+}
+
+function generateCard() {
+  const randomRank = getRandomRank();
+  document.getElementById("middle").textContent = randomRank;
+
+  const randomSuit = getRandomSuit();
+  document.getElementById("top").textContent = randomSuit;
+  document.getElementById("bottom").textContent = randomSuit;
+
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.getElementById("top").style.color = "red";
+    document.getElementById("bottom").style.color = "red";
+  } else if (randomSuit === "♠" || randomSuit === "♣") {
+    document.getElementById("top").style.color = "black";
+    document.getElementById("bottom").style.color = "black";
+  }
+}
+window.onload = generateCard;
+document.onclick = generateCard;
